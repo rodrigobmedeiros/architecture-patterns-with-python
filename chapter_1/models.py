7ab890cf3e77084d4e11aab2fbf282db7dae966a
Rodrigo Bernardo Medeiros <rbmedeiros@esss.co>
2022-12-29 09:36:04 -0300

    Change the structure of Batch class to keep the order lines

--- chapter_1/models.py
+++ chapter_1/models.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from datetime import date
 from typing import Optional
+from typing import Set
 
 __all__ = [
     'Batch',
@@ -19,11 +20,31 @@ class Batch:
 
         self.reference = reference
         self.sku = sku
-        self.avaiable_quantity = qty
         self.eta = eta 
+        self._purchased_quantity = qty
+        self._allocations: Set[OrderLine] = set()
+    
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def avaiable_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
     
     def allocate(self, order_line: OrderLine):
+        if self.can_allocate(order_line):
+            self._allocations.add(order_line)
 
-        self.avaiable_quantity -= order_line.qty
+    def deallocate(self, order_line: OrderLine):
+        if order_line in self._allocations:
+            self._allocations.remove(order_line)
+
+    def can_allocate(self, order_line: OrderLine) -> bool:
+
+        same_sku: bool = self.sku == order_line.sku
+        has_avaiable_quantity: bool = self.avaiable_quantity >= order_line.qty
+
+        return same_sku & has_avaiable_quantity
 
 
