880946b0cb2b6fd8a1f579e62785e8e7d1104168
Rodrigo Bernardo Medeiros <rbmedeiros@esss.co>
2022-12-29 09:38:21 -0300

    Include tests to cover allocate and deallocate lines into batchs

--- chapter_1/test_batches.py
+++ chapter_1/test_batches.py
@@ -21,14 +21,16 @@ def make_batch_and_line(
     """
 
     return (
-        Batch('batch-001', sku, batch_qty),
+        Batch('batch-001', sku, batch_qty, eta=date.today()),
         OrderLine('order-123', sku, line_qty)
     )
 
 
 def test_allocating_to_a_batch_reduces_the_avaiable_quantity():
+    batch, line = make_batch_and_line("SMALL-TABLE", 20, 2)
+
     batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
-    line = OrderLine('order-ref', "SMALL-TABLE", 2)
+    line = OrderLine('order-123', "SMALL-TABLE", 2)
 
     batch.allocate(line)
 
@@ -37,3 +39,22 @@ def test_allocating_to_a_batch_reduces_the_avaiable_quantity():
 def test_can_allocate_if_avaiablae_greater_than_required():
     large_batch, small_line = make_batch_and_line('ELEGANT-LAMP', 20,2)
     assert large_batch.can_allocate(small_line)
+
+def test_can_allocate_if_avaiable_equal_to_required():
+    batch, line = make_batch_and_line('ELEGANT-LAMP', 2, 2)
+    assert batch.can_allocate(line)
+
+def test_cannot_allocate_if_avaiable_less_than_required():
+    batch, line = make_batch_and_line('ELEGANT-LAMP', 2, 20)
+    assert batch.can_allocate(line) is False
+
+def test_cannot_allocate_if_skus_do_not_match():
+    batch = Batch('batch-001', 'ELEGANT-LAMP', 20, date.today())
+    line = OrderLine('order-123', 'SMALL-TABLE', 2)
+
+    assert batch.can_allocate(line) is False
+
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocatted_line = make_batch_and_line('DECORATIVE-TRINKET', 20, 2)
+    batch.deallocate(unallocatted_line)
+    assert batch.avaiable_quantity == 20
